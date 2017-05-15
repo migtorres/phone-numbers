e7b7cb666be5c72da59c18a604f35ee57e46ec97
migtorres <migtorres@gmail.com>
2017-05-15 18:36:47 +0100

    adds prefix list

--- prefix_counter.rb
+++ prefix_counter.rb
@@ -1,41 +1,36 @@
 require 'byebug'
-
+# Counts prefixes from a given array of numbers and
+# a given array of existing prefixes
 class PrefixCounter
-	attr_accessor :numbers
-	def self.results(numbers)
-		@prefix_count = {}
-		numbers.each do |number|
-			number = extra_characters(number)
-      number = number.gsub(/\s+/, "") if number
+  attr_accessor :numbers
+  def self.results(numbers, prefix_list)
+    @prefix_count = {}
+    numbers.each do |number|
+      number = extra_characters(number)
+      number = number.gsub(/\s+/, '') if number
       next unless length_condition(number) && check_just_digits(number)
-      prefix = prefix(number)
-			sum_values(prefix)
-		end
-		@prefix_count
-	end
+      match_prefixes(number, prefix_list)
+    end
+    @prefix_count
+  end
 
-	def self.extra_characters(number)
-		if number[0] == '+' && number[1..2] != '00' && number[1] != ' '
-			remove_plus(number)
-		elsif number[0..1] == '00'
-			remove_double_zero(number)
+  def self.extra_characters(number)
+    if number[0] == '+' && number[1..2] != '00' && number[1] != ' '
+      remove_plus(number)
+    elsif number[0..1] == '00'
+      remove_double_zero(number)
     else
       number
     end
-	end
-
-	def self.sum_values(prefix)
-    @prefix_count[prefix] ||= 0
-    @prefix_count[prefix] += 1
   end
 
-	def self.remove_plus(number)
-		number[1..-1]
-	end
+  def self.remove_plus(number)
+    number[1..-1]
+  end
 
-	def self.remove_double_zero(number)
-		number[2..-1]
-	end
+  def self.remove_double_zero(number)
+    number[2..-1]
+  end
 
   def self.length_condition(number)
     number.length == 3 || (number.length >= 7 && number.length <= 12) if number
@@ -45,7 +40,14 @@ class PrefixCounter
     number.scan(/\D/).empty?
   end
 
-	def self.prefix(number)
-		number[0..2]
-	end
+  def self.match_prefixes(number, prefix_list)
+    prefix_list.each do |prefix|
+      sum_values(prefix) if number.start_with?(prefix)
+    end
+  end
+
+  def self.sum_values(prefix)
+    @prefix_count[prefix] ||= 0
+    @prefix_count[prefix] += 1
+  end
 end
--- spec/prefix_counter_spec.rb
+++ spec/prefix_counter_spec.rb
@@ -3,45 +3,52 @@ require 'spec_helper'
 require_relative '../prefix_counter'
 
 describe 'prefix_counter' do
+  prefix_list = %w(351 994 21 352 353 354)
   it 'should accept and count 3 regular numbers' do
     input = ['+351912345678', '00351961234567', '00994123456789']
 
-    expect(PrefixCounter.results(input)).to eq({ '351' => 2, '994' => 1 })
+    expect(PrefixCounter.results(input, prefix_list)).to eq({ '351' => 2, '994' => 1 })
   end
 
   it 'should just count numbers with 3 or between 7 and 12 digits' do
     input = ['+351', '003511234', '00351123456789', '+21', '+3523', '00353123', '+3541234567890']
 
-    expect(PrefixCounter.results(input)).to eq({ '351' => 3 })
+    expect(PrefixCounter.results(input, prefix_list)).to eq({ '351' => 3 })
   end
 
   it 'does not count +00 as a valid number' do
     input = ['+351', '+00351911234567']
 
-    expect(PrefixCounter.results(input)).to eq({ '351' => 1 })
+    expect(PrefixCounter.results(input, prefix_list)).to eq({ '351' => 1 })
   end
 
   it 'does not count numbers with letters or special characters' do
     input = ['+351', '00351ABCD34567', '00351$%+#34567']
 
-    expect(PrefixCounter.results(input)).to eq({ '351' => 1 })
+    expect(PrefixCounter.results(input, prefix_list)).to eq({ '351' => 1 })
   end
 
   it 'does not count spaces between + and other numbers' do
     input = ['+351', '+ 351123456']
 
-    expect(PrefixCounter.results(input)).to eq({ '351' => 1 })
+    expect(PrefixCounter.results(input, prefix_list)).to eq({ '351' => 1 })
   end
 
    it 'accepts spaces between numbers' do
     input = ['+351', '+351 123456789']
 
-    expect(PrefixCounter.results(input)).to eq({ '351' => 2 })
+    expect(PrefixCounter.results(input, prefix_list)).to eq({ '351' => 2 })
   end
 
   it 'accepts numbers without + or 00 sign' do
     input = ['+351', '351123456789']
 
-    expect(PrefixCounter.results(input)).to eq({ '351' => 2 })
+    expect(PrefixCounter.results(input, prefix_list)).to eq({ '351' => 2 })
+  end
+
+  it 'only accepts numbers within prefix list' do
+    input = ['+351', '359123456']
+
+    expect(PrefixCounter.results(input, prefix_list)).to eq({ '351' => 1 })
   end
 end
